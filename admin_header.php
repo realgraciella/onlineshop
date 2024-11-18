8ba0ab0ba852f92cf8a5b6c444f48ebf08586b41
realgraciella <amariesye@gmail.com>
2024-11-18 14:48:30 +0800

    removed some pics

--- admin_header.php
+++ admin_header.php
@@ -3,52 +3,66 @@
 ?>
 
 <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top header-inner-pages">
-    <div class="container d-flex align-items-center justify-content-lg-between">
+<header id="header" class="fixed-top header-inner-pages">
+  <div class="container d-flex align-items-center justify-content-lg-between">
 
-    <a href="admin.php" class="logo me-auto me-lg-0"><img src="assets\img\logo\l1.png" alt="" class="img-fluid"></a>
+    <a href="admin.php" class="logo me-auto me-lg-0"><img src="assets/img/logo/l1.png" alt="" class="img-fluid"></a>
 
-      <nav id="navbar" class="navbar order-last order-lg-0">
-        <ul>
-          <li><a class="nav-link scrollto " href="admin.php">HOME</a></li>
-          <li><a class="nav-link scrollto " href="admin_products.php">BROCHURE</a></li>
-          <li class='dropdown'><a href='#'><span>PRODUCTS</span></a>
-            <ul>
-              <li ><a href='add_brand.php'><span>Add Brand</span></a></li>
-              <li ><a href='add_category.php'><span>Add Category</span></a></li>
-              <li ><a href='add_product.php'><span>Add Product</span></a></li>
-              <li ><a href='admin_productList.php'><span>Product List</span></a></li>
-            </ul>
-          </li>
-          <li class='dropdown'><a href='#'><span>AGENTS</span></a>
-            <ul>
-              <li ><a href='admin_registerAgent.php'><span>Add Sales Agent</span></a></li>
-              <li ><a href='admin_viewAgent.php'><span>View Sales Agent</span></a></li>
-              <li ><a href='admin_agentInfo.php'><span>Sales Agent Information</span></a></li>
-            </ul>
-          </li>
-          <li class='dropdown'><a href='#'><span>ORDERS</span></a>
-            <ul>
-              <li ><a href='admin_.php'><span>Order List</span></a></li>
-              <li ><a href='admin_.php'><span>Wishlist</span></a></li>
-              <li ><a href='admin_.php'><span>Check Out</span></a></li>
-            </ul>
-          </li>         
-          <li class='dropdown'><a href='#'><span>MESSAGES</span></a>
-            <ul>
-              <li ><a href='admin_viewInquiries.php'><span>Inquiries</span></a></li>
-              <li ><a href='admin_viewFeedbacks.php'><span>Feedbacks</span></a></li>
-            </ul>
-          </li>
-          <li class='dropdown'><a href='#'><span>PROFILE</span></a>
-            <ul>
-              <li ><a href='reset_pass.php'><span>Change Password</span></a></li>
-              <li ><a href='login.php'><span>Logout</span></a></li>
-            </ul>
-          </li>
-        </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
-      </nav><!-- .navbar -->
+    <nav id="navbar" class="navbar order-last order-lg-0">
+      <ul>
+        <li><a class="nav-link scrollto" href="admin.php">HOME</a></li>
+        <li><a class="nav-link scrollto" href="admin_products.php">BROCHURE</a></li>
+        <li class='dropdown'><a href='#'><span>PRODUCTS</span></a>
+          <ul>
+            <li><a href='add_brand.php'><span>Add Brand</span></a></li>
+            <li><a href='add_category.php'><span>Add Category</span></a></li>
+            <li><a href='add_product.php'><span>Add Product</span></a></li>
+            <li><a href='admin_productList.php'><span>Product List</span></a></li>
+          </ul>
+        </li>
+        <li class='dropdown'><a href='#'><span>AGENTS</span></a>
+          <ul>
+            <li><a href='admin_registerAgent.php'><span>Add Sales Agent</span></a></li>
+            <li><a href='admin_viewAgent.php'><span>View Sales Agent</span></a></li>
+            <li><a href='admin_agentInfo.php'><span>Sales Agent Information</span></a></li>
+          </ul>
+        </li>
+        <li class='dropdown'><a href='#'><span>ORDERS</span></a>
+          <ul>
+            <li><a href='admin_.php'><span>Order List</span></a></li>
+            <li><a href='admin_.php'><span>Wishlist</span></a></li>
+            <li><a href='admin_.php'><span>Check Out</span></a></li>
+          </ul>
+        </li>
+        <li class='dropdown'><a href='#'><span>MESSAGES</span></a>
+          <ul>
+            <li><a href='admin_viewInquiries.php'><span>Inquiries</span></a></li>
+            <li><a href='admin_viewFeedbacks.php'><span>Feedbacks</span></a></li>
+          </ul>
+        </li>
+        <li class='dropdown'><a href='#'><span>PROFILE</span></a>
+          <ul>
+            <li><a href='reset_pass.php'><span>Change Password</span></a></li>
+            <li><a href='login.php'><span>Logout</span></a></li>
+          </ul>
+        </li>
+      </ul>
+      <i class="bi bi-list mobile-nav-toggle"></i>
+    </nav><!-- .navbar -->
 
-    </div>
-  </header><!-- End Header -->
+  </div>
+</header><!-- End Header -->
+
+<!-- ======= JavaScript for Toggle ======= -->
+<script>
+  // Toggle Navbar for Mobile View
+  document.addEventListener('DOMContentLoaded', () => {
+    const mobileNavToggle = document.querySelector('.mobile-nav-toggle');
+    const navbar = document.querySelector('#navbar');
+
+    mobileNavToggle.addEventListener('click', () => {
+      navbar.classList.toggle('navbar-mobile');
+      mobileNavToggle.classList.toggle('bi-x'); // Change the icon on toggle
+    });
+  });
+</script>
